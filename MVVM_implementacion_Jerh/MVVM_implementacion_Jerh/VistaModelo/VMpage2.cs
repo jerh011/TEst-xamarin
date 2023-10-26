060f846303f203c7393b25bddf616bd9182aa8a7
jerh011 <jeelrohe@gmail.com>
2023-10-26 01:51:05 -0700

    Práctica 13

--- MVVM_implementacion_Jerh/MVVM_implementacion_Jerh/VistaModelo/VMpage2.cs
+++ MVVM_implementacion_Jerh/MVVM_implementacion_Jerh/VistaModelo/VMpage2.cs
@@ -2,22 +2,25 @@
 using System;
 using System.Collections.Generic;
 using System.Text;
+using MVVM_implementacion_Jerh.Modelo;
 using System.Threading.Tasks;
 using System.Windows.Input;
 using Xamarin.Forms;
 
 namespace MVVM_implementacion_Jerh.VistaModelo
 {
-    public class VMpage2: BaseViewModel
+    public class VMpage2 : BaseViewModel
     {
         #region VARIABLES
         string _Texto;
+        public List<Musuarios> listausuarios{ get; set; }
         #endregion
 
         #region Contructor
         public VMpage2(INavigation navigation)
         {
             Navigation = navigation;
+            Mostrarusuarios();
         }
         #endregion
 
@@ -38,10 +41,33 @@ namespace MVVM_implementacion_Jerh.VistaModelo
         {
 
         }
+        public void Mostrarusuarios() {
+            listausuarios = new List<Musuarios>
+            {
+                new Musuarios
+                {
+                    Nombre="Frank",Imagen="https://i.ibb.co/zGtXvry/conejo-de-pascua.png"
+                },
+                new Musuarios
+                {
+                    Nombre="Juan",Imagen="https://i.ibb.co/6WyXntz/conejo-2.png"
+                },
+                new Musuarios
+                {
+                    Nombre="Carlos",Imagen="https://i.ibb.co/Jxp1WGb/conejo-1.png"
+                },
+                new Musuarios
+                {
+                    Nombre="Eloy",Imagen="https://i.ibb.co/rx66K3W/conejo.png"
+                }
+            };
+        
+        
+        }
         #endregion.
         #region CONTRUCTOR
         public ICommand Volvercommand => new Command(async () => await Volver());
-        public ICommand ProcesoSimpCommand => new Command(ProcesoSimple);
+        //public ICommand ProcesoSimpCommand => new Command(ProcesoSimple);
         #endregion.
     }
 }
